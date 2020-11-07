22ed6a5e21691658bd9281518de78ce85db83fed
buda@786 <sulemansk.in@gmail.com>
2020-11-07 09:42:27 +0530

    one programe change

--- src/PART_26_Java_Programe_CountOccurencesofaCharacterInString/CountOccurencesString.java
+++ src/PART_26_Java_Programe_CountOccurencesofaCharacterInString/CountOccurencesString.java
@@ -4,12 +4,12 @@ public class CountOccurencesString {
 
 	public static void main(String[] args) {
         
-		String str="Java programming java oops";
+		String str="java programming java oops";
 		
 		int totcount=str.length();
 
 		
-		int totalcount_afterremove=str.replace("a", "").length();
+		int totalcount_afterremove=str.replace("o", "").length();
 		
 		int count=totcount-totalcount_afterremove;
 		System.out.println(count);
